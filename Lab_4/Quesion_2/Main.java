e064659267b036867917fadcbe4dacdbb00d292e
Robeil Aregawi <robeilaregawi1@gmail.com>
2021-10-02 21:59:10 -0500

    Fixed innt out put from double

--- Lab_4/Quesion_2/Main.java
+++ Lab_4/Quesion_2/Main.java
@@ -1,24 +1,15 @@
 package CS201_Assignments.Lab_4.Quesion_2;
 
-import Assignment.Lab_4.WeightConverter;
-
-import java.text.DecimalFormat;
+import Assignment.Lab_4.CoffeeBag;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Import decimal format to edit the output decimal
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        //Created bag for sale object
+        Assignment.Lab_4.CoffeeBag bagForSale = new CoffeeBag(5,32);
 
-        //created moon weight object
-        WeightConverter moonWeight = new WeightConverter();
-
-        //calling the  weight converter to convert the given weight
-        double yourWeight = moonWeight.convert(191);
-
-        //Printing the result to console
-        System.out.println("This is your weight in moon " + decimalFormat.format(yourWeight));
-
+        //Printing out to the console
+        System.out.println(bagForSale.toString());
 
     }
 }
